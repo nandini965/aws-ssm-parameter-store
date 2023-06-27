94605ed3af6fff378e89772739f6c2d9224d1395
nandini965 <nandhini965@gmail.com>
2023-06-28 00:22:14 +0530

    main.yml

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "aws_ssm_parameter" "parameter"
+resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  value = "hello world"
-  type = "securestring"
+  value = "Hello world"
+  type      = "securestring"
   key_id = "7c7e07df-6842-43fe-aad3-b3d8a9131425"
 }
