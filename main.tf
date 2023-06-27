e89545634d18edc64752f556218f211848ae3eb6
nandini965 <nandhini965@gmail.com>
2023-06-27 21:46:03 +0530

    main.yml

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
-  name  = var.count[index].name
-  value = var.count[index].value
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
   type = "string"
   key-id = "7c7e07df-6842-43fe-aad3-b3d8a9131425"
 }
