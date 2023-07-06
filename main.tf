3b8e33ceaa2503e25c233ecab061c1e993997add
nandini965 <nandhini965@gmail.com>
2023-07-06 17:27:51 +0530

    shipping

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ resource "aws_ssm_parameter" "passwords" {
   count = length(var.passwords)
   name  = var.passwords[count.index].name
   value = var.passwords[count.index].value
-  type      = "secureString"
+  type      = "SecureString"
   key_id = "7c7e07df-6842-43fe-aad3-b3d8a9131425"
 }
