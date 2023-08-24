805c602e6ffdf067e3b11c43987170ec00ef936e
nandini965 <nandhini965@gmail.com>
2023-08-24 14:37:52 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name  = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type      = "String"
-  key_id = "7c7e07df-6842-43fe-aad3-b3d8a9131425"
+  key_id = "e4d7c861-f016-44b5-8edd-3e79dfdda561"
 }
 
 
@@ -12,5 +12,5 @@ resource "aws_ssm_parameter" "passwords" {
   name  = var.passwords[count.index].name
   value = var.passwords[count.index].value
   type      = "SecureString"
-  key_id = "7c7e07df-6842-43fe-aad3-b3d8a9131425"
+  key_id = "e4d7c861-f016-44b5-8edd-3e79dfdda561"
 }
