d0cb87c9e1bc5c823b9d55bc435113b990301199
nandini965 <nandhini965@gmail.com>
2024-02-23 00:39:43 +0530

    filebeat

--- init-container.sh
+++ init-container.sh
@@ -1,1 +1,6 @@
-aws ssm get-parameters  --names prod.payment.amqp_host | jq .Parameters[].Value | sed -e 's/"//g'
+for param in ${params}; do
+Var=$(echo $param | awk -F . '{print $NF}' tr '[:lower:]' '[:upper:]' )
+VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g'
+echo $VAR=$VAL
+echo $VAR=$VAL >>/params/params
+done
