46acbe969d6adb3d650280f76b37395910fe92db
nandini965 <nandhini965@gmail.com>
2024-02-25 18:01:01 +0530

    filebeat

--- init-container.sh
+++ init-container.sh
@@ -1,6 +1,6 @@
 for param in ${params}; do
 Var=$(echo $param | awk -F . '{print $NF}' tr '[:lower:]' '[:upper:]' )
-VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g'
+VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g')
 echo $VAR=$VAL
 echo $VAR=$VAL >>/params/params
 done
