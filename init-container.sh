2d0b6bd70042fc45d94a61017618f320f9442304
nandini965 <nandhini965@gmail.com>
2024-02-27 20:35:48 +0530

    filebeat

--- init-container.sh
+++ init-container.sh
@@ -1,6 +1,6 @@
-for param in ${params}; do
-Var=$(echo $param | awk -F . '{print $NF}' tr '[:lower:]' '[:upper:]' )
-VAL=$(aws ssm get-parameters  --names $param --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
-echo export $VAR=\"$VAL\"
-echo export $VAR=$\"$VAL\" >>/params/params
+for param in ${PARAMS}; do
+  VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]' )
+  VAL=$(aws ssm get-parameters  --names $param --with-decryption | jq .Parameters[].Value | sed -e 's/"//g')
+  echo export $VAR=\"$VAL\"
+  echo export $VAR=\"$VAL\" >>/params/params
 done
